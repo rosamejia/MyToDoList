e066cf1519dc6bc009b22177cbdb1dd0f0409e12
Rosa Mejia <rosa_m_mejia@macbook-pro.local>
2019-01-04 09:59:22 -0500

    Initial Commit

---
+++ MyToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyToDoList
+//
+//  Created by Rosa Mejia on 1/4/19.
+//  Copyright © 2019 Rosa Mejia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
