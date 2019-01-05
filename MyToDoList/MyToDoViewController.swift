1452bee08aab2d37f1dbd6bd2fa41e313c5321ab
Rosa Mejia <rosa_m_mejia@macbook-pro.local>
2019-01-05 08:06:26 -0500

    Initial Load

--- MyToDoList/MyToDoViewController.swift
+++ MyToDoList/MyToDoViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MyToDoViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("did load")
     }
 
 
