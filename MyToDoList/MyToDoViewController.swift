b9e2bfa1c1e9b5a9779e12a53052d1ddd9f132b5
Rosa Mejia <rosa_m_mejia@macbook-pro.local>
2019-01-05 08:34:21 -0500

    display values on list

--- MyToDoList/MyToDoViewController.swift
+++ MyToDoList/MyToDoViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class MyToDoViewController: UITableViewController {
 
+    let itemArray = ["Item 1", "Item 2", "Item 3"]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        print(itemArray[indexPath.row])
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
