4944a851c54d346e06df7f761d91280e0cbe91cf
Rosa Mejia <rosa_m_mejia@macbook-pro>
2019-01-06 08:58:53 -0600

    end of lesson 230

--- MyToDoList/Controllers/MyToDoViewController.swift
+++ MyToDoList/Controllers/MyToDoViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class MyToDoViewController: UITableViewController {
 
-    var itemArray = ["Item 1", "Item 2", "Item 3"]
+    var itemArray = [Item]()
     var defaults = UserDefaults.standard
     
     
@@ -21,16 +21,38 @@ class MyToDoViewController: UITableViewController {
     //MARK table view source
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
-        //print(itemArray[indexPath.row])
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
+//      or cell.accessoryType = item.done == true ? .checkmark : .none
+//      or
+//        if item.done == true{
+//            cell.accessoryType = .checkmark
+//        }
+//        else {
+//            cell.accessoryType = .none
+//        }
+        
         return cell
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("did load")
-        if let items = defaults.array(forKey: "MyToDoListArray") as? [String]{
+        //print("did load")
+//        if let items = defaults.array(forKey: "MyToDoListArray") as? [String]{
+//            itemArray = items
+//        }
+        let newItem = Item()
+        newItem.title = "Item 1"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Item 2"
+        itemArray.append(newItem2)
+        
+        if let items = defaults.array(forKey: "MyToDoListArray") as? [Item]{
             itemArray = items
         }
     }
@@ -38,13 +60,10 @@ class MyToDoViewController: UITableViewController {
     //MARK table view delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print("selected row" + itemArray[indexPath.row])
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
         
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -55,7 +74,10 @@ class MyToDoViewController: UITableViewController {
         let alert = UIAlertController(title: "Add new Item?", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user clicks the Add Item button on our UIAlert
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
             self.defaults.set(self.itemArray, forKey: "MyToDoListArray") 
             
             self.tableView.reloadData()
---
+++ MyToDoList/Data Model/ItemList.swift
@@ -0,0 +1,20 @@
+//
+//  ItemList.swift
+//  MyToDoList
+//
+//  Created by Rosa Mejia on 1/6/19.
+//  Copyright © 2019 Rosa Mejia. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title: String = ""
+    var done: Bool = false
+    
+//    init(itemText:String,checked:Bool) {
+//        title = itemText
+//        done = checked
+//    }
+    
+}
