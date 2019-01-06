6a0b966d4e81bb92eb0d5dc064b536906104b19b
Rosa Mejia <rosa_m_mejia@macbook-pro.local>
2019-01-06 10:23:29 -0600

    End of lesson 234

--- MyToDoList/Controllers/MyToDoViewController.swift
+++ MyToDoList/Controllers/MyToDoViewController.swift
@@ -13,7 +13,28 @@ class MyToDoViewController: UITableViewController {
     var itemArray = [Item]()
     var defaults = UserDefaults.standard
     
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(dataFilePath)
+        
+        loadItems()
+        
+//        let newItem = Item()
+//        newItem.title = "Item 1"
+//        itemArray.append(newItem)
+//
+//        let newItem2 = Item()
+//        newItem2.title = "Item 2"
+//        itemArray.append(newItem2)
+        
+//        if let items = defaults.array(forKey: "MyToDoListArray") as? [Item]{
+//            itemArray = items
+//        }
+    }
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -37,35 +58,17 @@ class MyToDoViewController: UITableViewController {
         return cell
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        //print("did load")
-//        if let items = defaults.array(forKey: "MyToDoListArray") as? [String]{
-//            itemArray = items
-//        }
-        let newItem = Item()
-        newItem.title = "Item 1"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "Item 2"
-        itemArray.append(newItem2)
-        
-        if let items = defaults.array(forKey: "MyToDoListArray") as? [Item]{
-            itemArray = items
-        }
-    }
 
     //MARK table view delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print("selected row" + itemArray[indexPath.row])
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveItems()
         
-        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
     
     //MARK Add new items
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
@@ -78,9 +81,9 @@ class MyToDoViewController: UITableViewController {
             newItem.title = textField.text!
             
             self.itemArray.append(newItem)
-            self.defaults.set(self.itemArray, forKey: "MyToDoListArray") 
+            self.saveItems()
+            //self.defaults.set(self.itemArray, forKey: "MyToDoListArray")
             
-            self.tableView.reloadData()
         }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
@@ -88,6 +91,29 @@ class MyToDoViewController: UITableViewController {
         }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    func loadItems(){
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+        }
+    }
+    
+        
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        tableView.reloadData()
     }
     
 }
