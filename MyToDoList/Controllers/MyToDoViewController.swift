904c2fee95100affcf2873bcde5fc889e65e56ed
Rosa Mejia <rosa_m_mejia@macbook-pro>
2019-01-07 07:26:50 -0600

    end of lesson  244 search bar clear, reload all items

--- MyToDoList/Controllers/MyToDoViewController.swift
+++ MyToDoList/Controllers/MyToDoViewController.swift
@@ -130,7 +130,15 @@ extension MyToDoViewController: UISearchBarDelegate {
         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
         
         loadItems(with: request)
-        
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
     }
 }
 
