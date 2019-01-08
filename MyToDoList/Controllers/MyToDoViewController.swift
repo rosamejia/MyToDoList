c2371f7aa8cbf269c1cdcf89a720d7d80d9e0fcf
Rosa Mejia <rosa_m_mejia@macbook-pro.local>
2019-01-08 07:26:36 -0600

    end of lesson 247, category and items are related in the database and we can save and retrieve them

--- MyToDoList/Controllers/MyToDoViewController.swift
+++ MyToDoList/Controllers/MyToDoViewController.swift
@@ -12,6 +12,11 @@ import CoreData
 class MyToDoViewController: UITableViewController {
 
     var itemArray = [Item]()
+    var selectedCategory : Category? {
+        didSet {
+            loadItems()
+        }
+    }
     var defaults = UserDefaults.standard
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
@@ -19,8 +24,8 @@ class MyToDoViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
-        
-        loadItems()
+        //removed the loadItems because above selectedCategory will loadItems if a category exist
+        //loadItems()
         
 //        let newItem = Item()
 //        newItem.title = "Item 1"
@@ -38,6 +43,7 @@ class MyToDoViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
+    
     
     //MARK table view source
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -58,7 +64,7 @@ class MyToDoViewController: UITableViewController {
         return cell
     }
     
-
+    
     //MARK table view delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print("selected row" + itemArray[indexPath.row])
@@ -83,7 +89,7 @@ class MyToDoViewController: UITableViewController {
             let newItem = Item(context: self.context)
             newItem.title = textField.text!
             newItem.done = false
-            
+            newItem.parentCategory = self.selectedCategory
             self.itemArray.append(newItem)
             self.saveItems()
             //self.defaults.set(self.itemArray, forKey: "MyToDoListArray")
@@ -97,7 +103,15 @@ class MyToDoViewController: UITableViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()){
+    //MARK: - Data Manipulation Methods
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil){
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        } else {
+            request.predicate = categoryPredicate
+        }
+        
         do{
             itemArray = try context.fetch(request)
         } catch {
@@ -122,14 +136,11 @@ class MyToDoViewController: UITableViewController {
 extension MyToDoViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         let request : NSFetchRequest<Item> = Item.fetchRequest()
-//        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
-//        request.predicate = predicate
-        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
-//        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
-//        request.sortDescriptors = [sortDescriptor]
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+
         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
         
-        loadItems(with: request)
+        loadItems(with: request, predicate: predicate)
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
